f61b802e7d70805e135df3273ae4b12c5e374b0a
Anna Kirillova <m2505004@edu.misis.ru>
2025-10-04 20:20:02 +0300

    task 1

---
+++ lab2/arrays.py
@@ -0,0 +1,39 @@
+""" nums = []
+n = int(input())
+for i in range(n):
+    s = input()
+    if '.' in s:
+        number = float(s)
+    else:
+        number = int(s)
+    nums.append(number)
+print(nums)
+
+def min_max(nums):
+    if len(nums) == 0:
+        return "ValueError"
+    return (min(nums), int(max(nums)))
+
+def unique_sorted(nums):
+    return sorted(set(nums))
+print(unique_sorted(nums)) """
+
+def flatten(mat):
+    listt = []
+    for sublist in mat:
+        for item in sublist:
+            if not item.isdigit():
+                return 'TypeError'
+            else:
+                listt.append(int(item))
+    return listt
+
+n = int(input("Сколько списков вы хотите создать? "))
+mat = []
+for i in range(n):
+    elements = input(f"Введите числа для списка {i + 1} через пробел: ")
+    inner_list = elements.split()
+    mat.append(inner_list)
+
+print(mat)
+print(flatten(mat))
