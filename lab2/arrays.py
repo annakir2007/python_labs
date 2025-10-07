527b3b088c8ea9efc8693da6041d29d309a087d9
Anna Kirillova <m2505004@edu.misis.ru>
2025-10-07 13:11:37 +0300

    arrays

--- lab2/arrays.py
+++ lab2/arrays.py
@@ -1,4 +1,4 @@
-nums = []
+'''nums = []
 n = int(input())
 for i in range(n):
     s = input()
@@ -15,25 +15,41 @@ def min_max(nums):
     return (min(nums), int(max(nums)))
 
 def unique_sorted(nums):
-    return sorted(set(nums))
+    return sorted(set(nums))'''
 
 def flatten(mat):
+    # Если вход - кортеж, преобразуем его в список
+    if isinstance(mat, tuple):
+        mat = list(mat)
+
     listt = []
-    for sublist in mat:#внутренние списки
-        for item in sublist:#по элементам
-            if not item.isdigit():#проверка на все числа
-                return 'TypeError'
-            else:
-                listt.append(int(item))
+    for sublist in mat:  # могут быть и списки, и кортежи
+        # Определяем возможные вложенные кортежи/списки
+        if isinstance(sublist, (list, tuple)):
+            for item in sublist:
+                # Проверка, является ли item числом или строкой
+                if isinstance(item, str):#если элемент - строка
+                    if not item.isdigit():
+                        return 'TypeError'
+                    else:
+                        listt.append(int(item))
+                elif isinstance(item, (int, float)):#если элемент - число
+                    listt.append(item)
+                else:
+                    return 'TypeError'
+        else:
+            # Если внутри не список/кортеж, возможно, ошибка
+            return 'TypeError'
     return listt
 
-n = int(input("Сколько списков вы хотите создать? "))
+'''n = int(input("Сколько списков вы хотите создать? "))
 mat = []
 for i in range(n):
     elements = input(f"Введите числа для списка {i + 1} через пробел: ")
     inner_list = elements.split()
-    mat.append(inner_list)
-
-print(min_max(nums))
+    mat.append(inner_list)'''
+print(flatten([(1, 2), (3, 4)]))
+print(flatten([[1, 2], [3, 4]]))
+'''print(min_max(nums))
 print(unique_sorted(nums))
-print(flatten(mat))
+print(flatten(mat))'''
